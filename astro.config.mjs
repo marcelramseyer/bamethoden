84eb5c02cae17beb9c402a49d4207d7035144278
Marcel Ramseyer <marcel.ramseyer@hispeed.ch>
2026-02-11 11:19:55 +0100

    style: just played around with some ui styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,37 +4,49 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'BA Methods',
-			description: 'Meine Dokumentation über BA Methoden',
-			defaultLocale: 'de',
-			locales: {
-				root: {
-					label: 'Deusch',
-					lang: 'de'
-				}
-			},
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Richtlinien',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-				{
-					label: 'Prototyping',
-					autogenerate: { directory: 'test' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: '❖ Methodensammlung',
+      description: 'Meine Dokumentation über BA Methoden',
+      defaultLocale: 'de',
+      components: {
+        ThemeSelect: './src/components/ThemeSelect.astro',
+      },
+      customCss: [
+        // Relativer Pfad zu deiner benutzerdefinierten CSS-Datei
+        './src/styles/custom.css',
+      ],
+
+      // logo: {
+      //   light: './src/assets/light-logo.svg',
+      //   dark: './src/assets/dark-logo.svg',
+      // },
+      locales: {
+        root: {
+          label: 'Deusch',
+          lang: 'de',
+        },
+      },
+      social: {
+        github: 'https://github.com/withastro/starlight',
+      },
+      sidebar: [
+        {
+          label: 'Richtlinien',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+        {
+          label: 'Prototyping',
+          autogenerate: { directory: 'test' },
+        },
+      ],
+    }),
+  ],
 });
---
+++ src/content/docs/test/test3.md
@@ -0,0 +1,15 @@
+---
+title: test3
+description: test3
+tableOfContents: false
+---
+
+mein Text
+
+```js
+// Javascript-Code mit Syntaxhervorhebung.
+var fun = function lang(l) {
+  dateformat.i18n = require('./lang/' + l);
+  return true;
+};
+```
